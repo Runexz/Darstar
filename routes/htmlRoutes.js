8e06fe2d27f521b2ec4dc434227aaa972de1162b
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-09-26 20:53:14 -0500

    deleted handlebar info

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,22 +1,17 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Classroom.findAll({}).then(function(dbClassroom) {
+  app.get("/", function (req, res) {
+    db.Classroom.findAll({}).then(function (dbClassroom) {
       res.json(dbClassroom);
-      });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/Classroom/:id", function(req, res) {
-    db.Classroom.findOne({ where: { id: req.params.id } }).then(function(dbClassroom) {
-      res.json(dbClassroom)
     });
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  // Load example page and pass in an example by id
+  app.get("/Classroom/:id", function (req, res) {
+    db.Classroom.findOne({ where: { id: req.params.id } }).then(function (dbClassroom) {
+      res.json(dbClassroom)
+    });
   });
 };
