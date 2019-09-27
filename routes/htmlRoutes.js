64b6558bb45695bbf8104f441c492fc0d9bfa03a
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-09-26 20:39:48 -0500

    got rid of handlebar info

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,20 +3,15 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+    db.Classroom.findAll({}).then(function(dbClassroom) {
+      res.json(dbClassroom);
       });
-    });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+  app.get("/Classroom/:id", function(req, res) {
+    db.Classroom.findOne({ where: { id: req.params.id } }).then(function(dbClassroom) {
+      res.json(dbClassroom)
     });
   });
 
