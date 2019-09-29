1be84ebd24029e8f487c8cf0c39c6f613b8da96a
Stama Eleazar <eleoatam10@gmail.com>
2019-09-28 19:33:24 -0500

    added style to all html files. all working

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -1,236 +1,235 @@
 
-    $(document).ready(function () {
-      console.log("document is ready!");
-      $('.teaBeLogTab').hide();
+$(document).ready(function () {
+  console.log("document is ready!");
+  $('.teaBeLogTab').hide();
 
-    });
-
-
-    //-----------------------------------------------------------------------------------------------
-
-    //create if statement to check that form and comment have been filled out completely
+});
 
 
-    //-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+
+//create if statement to check that form and comment have been filled out completely
 
 
-
-
-
-
-    $("#form").submit(function (event) {
-      var textarea = $("#inputPassword3");
-      if (textarea.val().trim() === "") {
-        // No message, add red highlighting to indicate error
-        textarea.css("box-shadow", "0 0 12px #811");
-        $('.teacherLogAppend').text("Please enter the student's ID")
-        event.preventDefault();
-
-      } else {
-
-        console.log('form is completed')
-
-        $('.jumbotron').hide();
-
-
-        /////////////////////////////////////////////////////////////////////////////////////
-
-        //display result of id search here ---- place to call ajax to call api for mysql
-
-        /////////////////////////////////////////////////////////////////////////////////////
-
-        $('.teaBeLogTab').show();
-
-
-
-        event.preventDefault();
-
-
-      }
-    });
-
-    disableBtn()
+//-----------------------------------------------------------------------------------------------
 
 
 
 
 
-    function disableBtn() {
 
-      $(".teachBehavLog").attr("disabled", true)
+$("#form").submit(function (event) {
+  var textarea = $("#inputPassword3");
+  if (textarea.val().trim() === "") {
+    // No message, add red highlighting to indicate error
+    textarea.css("box-shadow", "0 0 12px #811");
+    $('.teacherLogAppend').text("Please enter the student's ID")
+    event.preventDefault();
 
-      $("#teachComment").on('click', function () {
+  } else {
 
-        var radioOne = $("input[name='inlineRadioOptions4']:checked").val()
-        var radioTwo = $("input[name='inlineRadioOptions3']:checked").val()
-        var radioThree = $("input[name='inlineRadioOptions2']:checked").val()
-        var radioFour = $("input[name='inlineRadioOptions1']:checked").val()
+    console.log('form is completed')
 
-        console.log(radioOne)
-        console.log(radioTwo)
-        console.log(radioThree)
-        console.log(radioFour)
-
-
-        
-      
-
-      var textboxOne = $("#teachComment").val().trim().length ;
-      console.log("boxlength is: " + textboxOne)
+    $('.jumbotron').hide();
 
 
+    /////////////////////////////////////////////////////////////////////////////////////
 
+    //display result of id search here ---- place to call ajax to call api for mysql
 
+    /////////////////////////////////////////////////////////////////////////////////////
 
-        if (parseInt(radioOne) >= 1 && parseInt(radioTwo) >= 1 && parseInt(radioThree) >= 1 && parseInt(radioFour) >= 1 && textboxOne >=2) {
-
-          console.log('true')
-
-          $(".teachBehavLog").attr("disabled", false);
+    $('.teaBeLogTab').show();
 
 
 
-        } else {
-
-          console.log('not selected')
-
-          $('.textCheckForm').text('Please complete the tracker above!')
+    event.preventDefault();
 
 
-        }
+  }
+});
 
-      })
+disableBtn()
+
+
+
+
+
+function disableBtn() {
+
+  $(".teachBehavLog").attr("disabled", true)
+
+  $("#teachComment").on('click', function () {
+
+    var radioOne = $("input[name='inlineRadioOptions4']:checked").val()
+    var radioTwo = $("input[name='inlineRadioOptions3']:checked").val()
+    var radioThree = $("input[name='inlineRadioOptions2']:checked").val()
+    var radioFour = $("input[name='inlineRadioOptions1']:checked").val()
+
+    console.log(radioOne)
+    console.log(radioTwo)
+    console.log(radioThree)
+    console.log(radioFour)
+
+
+
+
+
+    var textboxOne = $("#teachComment").val().trim().length;
+    console.log("boxlength is: " + textboxOne)
+
+
+
+
+
+    if (parseInt(radioOne) >= 1 && parseInt(radioTwo) >= 1 && parseInt(radioThree) >= 1 && parseInt(radioFour) >= 1 && textboxOne >= 2) {
+
+      console.log('true')
+
+      $(".teachBehavLog").attr("disabled", false);
+
+
+
+    } else {
+
+      console.log('not selected')
+
+      $('.textCheckForm').text('Please complete the tracker above!')
+
+
     }
 
+  })
+}
 
 
 
 
-    // button on-click for teacher submit behavior log
 
+// button on-click for teacher submit behavior log
 
-    $('.teachBehavLog').on('click', function (event) {
 
+$('.teachBehavLog').on('click', function (event) {
 
-      
 
 
-      var textbox = $("#teachComment");
-      if (textbox.val().trim() === "") {
 
-        event.preventDefault();
-        // No message, add red highlighting to indicate error
-        textbox.css("box-shadow", "0 0 12px #811");
 
-        console.log('clicked behavior log btn');
+  var textbox = $("#teachComment");
+  if (textbox.val().trim() === "") {
 
+    event.preventDefault();
+    // No message, add red highlighting to indicate error
+    textbox.css("box-shadow", "0 0 12px #811");
 
+    console.log('clicked behavior log btn');
 
-      } else {
 
-        console.log('form 2 is completed ')
 
+  } else {
 
-        // console.log('ready to submit 2')
+    console.log('form 2 is completed ')
 
 
-      }
+    // console.log('ready to submit 2')
 
 
+  }
 
 
 
-      // collecting data input from teacher
-      console.log('-----------------------------------------------------------------------------')
 
-      console.log('DATA TO SEND TO MYSQL BELOW')
 
-      console.log('-----------------------------------------------------------------------------')
+  // collecting data input from teacher
+  console.log('-----------------------------------------------------------------------------')
 
-      var radioValue = $("input[name='inlineRadioOptions4']:checked").val();
-      if (radioValue) {
-        console.log("perseverance: " + radioValue);
-      }
+  console.log('DATA TO SEND TO MYSQL BELOW')
 
-      var radioValue2 = $("input[name='inlineRadioOptions3']:checked").val();
-      if (radioValue2) {
-        console.log("Leadership: " + radioValue2);
-      }
+  console.log('-----------------------------------------------------------------------------')
 
-      var radioValue3 = $("input[name='inlineRadioOptions2']:checked").val();
-      if (radioValue3) {
-        console.log("Uplift: " + radioValue3);
-      }
-      var radioValue4 = $("input[name='inlineRadioOptions1']:checked").val();
-      if (radioValue4) {
-        console.log("Scholarship: " + radioValue4);
-      }
+  var radioValue = $("input[name='inlineRadioOptions4']:checked").val();
+  if (radioValue) {
+    console.log("perseverance: " + radioValue);
+  }
 
-      console.log('-----------------------------------------------------------------------------')
+  var radioValue2 = $("input[name='inlineRadioOptions3']:checked").val();
+  if (radioValue2) {
+    console.log("Leadership: " + radioValue2);
+  }
 
+  var radioValue3 = $("input[name='inlineRadioOptions2']:checked").val();
+  if (radioValue3) {
+    console.log("Uplift: " + radioValue3);
+  }
+  var radioValue4 = $("input[name='inlineRadioOptions1']:checked").val();
+  if (radioValue4) {
+    console.log("Scholarship: " + radioValue4);
+  }
 
-      var scoreTotal = (parseInt(radioValue) + parseInt(radioValue2) + parseInt(radioValue3) + parseInt(radioValue4));
+  console.log('-----------------------------------------------------------------------------')
 
 
-      var teacherCommment = $('#teachComment').val();
+  var scoreTotal = (parseInt(radioValue) + parseInt(radioValue2) + parseInt(radioValue3) + parseInt(radioValue4));
 
-      console.log("teacher comment: " + teacherCommment)
 
+  var teacherCommment = $('#teachComment').val();
 
+  console.log("teacher comment: " + teacherCommment)
 
 
-      console.log('-----------------------------------------------------------------------------')
 
-      var isHwChecked = $("#formWork input[type='radio']:checked").val();
-      // console.log(isHwChecked)
 
-      console.log('missing work: ' + isHwChecked)
+  console.log('-----------------------------------------------------------------------------')
 
-      console.log('-----------------------------------------------------------------------------')
+  var isHwChecked = $("#formWork input[type='radio']:checked").val();
+  // console.log(isHwChecked)
 
-      console.log("total score: " + scoreTotal)
+  console.log('missing work: ' + isHwChecked)
 
-      // action once data has been collected
+  console.log('-----------------------------------------------------------------------------')
 
-      var behaviorColor;
+  console.log("total score: " + scoreTotal)
 
-      if (scoreTotal > 18 && scoreTotal <= 20) {
+  // action once data has been collected
 
+  var behaviorColor;
 
-        behaviorColor = 'blue';
+  if (scoreTotal > 18 && scoreTotal <= 20) {
 
-      } else if (scoreTotal > 16 && scoreTotal <= 18) {
 
-        behaviorColor = 'Green';
+    behaviorColor = 'blue';
 
-      } else if (scoreTotal > 14 && scoreTotal <= 16) {
+  } else if (scoreTotal > 16 && scoreTotal <= 18) {
 
-        behaviorColor = 'Yellow';
+    behaviorColor = 'Green';
 
-      } else if (scoreTotal >= 13 && scoreTotal <= 14) {
+  } else if (scoreTotal > 14 && scoreTotal <= 16) {
 
-        behaviorColor = 'Orange';
+    behaviorColor = 'Yellow';
 
-      } else {
+  } else if (scoreTotal >= 13 && scoreTotal <= 14) {
 
-        behaviorColor = 'Red';
+    behaviorColor = 'Orange';
 
-      }
+  } else {
 
+    behaviorColor = 'Red';
 
+  }
 
-      console.log('color: ' + behaviorColor)
 
 
-      
+  console.log('color: ' + behaviorColor)
 
 
-console.log('-------------------------------------------------')
-      
 
 
-    })
 
+  console.log('-------------------------------------------------')
 
 
-  
+
+})
+
+
+
