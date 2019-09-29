1b99750275fa4f8c23184f3f16087b841d812bf2
Stama Eleazar <eleoatam10@gmail.com>
2019-09-28 19:10:31 -0500

    added stilyng to contact.html

--- models/schema.sql
+++ models/schema.sql
@@ -1,2 +1,17 @@
 DROP DATABASE IF EXISTS darstardb;
 CREATE DATABASE darstardb;
+
+CREATE TABLE Classroom
+(
+	id int NOT NULL AUTO_INCREMENT,
+	studentid INTEGER NOT NULL,
+    name varchar(100) NOT NULL,
+    pillar1 INTEGER NOT NULL,
+    pillar2 INTEGER NOT NULL,
+    pillar3 INTEGER NOT NULL,
+    pillar4 INTEGER NOT NULL,
+    color VARCHAR(100) NOT NULL,
+    date DATETIME NOT NULL,
+    descriptioncomments VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
