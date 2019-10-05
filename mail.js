a887bca2cd8ceaa99a8250468d27382662c5cdec
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-05 11:47:51 -0500

    mail works

--- mail.js
+++ mail.js
@@ -4,8 +4,8 @@ const mailGun = require('nodemailer-mailgun-transport');
 
 const auth = {
     auth: {
-        api_key: "d1b90b577df6ea63fb074c511d5ed164-af6c0cec-c745a6e9", // TODO: Replace with your mailgun API KEY
-        domain: "sandboxe6d94f87cefd4a62bcaa1da8c695c514.mailgun.org" // TODO: Replace with your mailgun DOMAIN
+        api_key: "1228ae5ac3c42061bd9c7874296f7763-af6c0cec-280de2f9", // TODO: Replace with your mailgun API KEY
+        domain: "sandboxda39828700f74a6c89adc92019b83fc8.mailgun.org" // TODO: Replace with your mailgun DOMAIN
     }
 };
 
@@ -15,7 +15,7 @@ const transporter = nodemailer.createTransport(mailGun(auth));
 const sendMail = (email, subject, text, cb) => {
     const mailOptions = {
         from: email,
-        to: 'darstarinc@gmail.com ',
+        to: 'daretobeastarme@gmail.com',
         subject,
         text
     };
